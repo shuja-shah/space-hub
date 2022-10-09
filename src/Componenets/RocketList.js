166b9c68d4db7777388dd58a5a86f79548153188
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-09 14:17:50 +0500

    Define Home Section

--- src/Componenets/RocketList.js
+++ src/Componenets/RocketList.js
@@ -10,16 +10,35 @@ const RocketList = () => {
     dispatch(getRockets());
   }, []);
   return (
-    <section className="Rocket-List">
-      {rockets.map((item) => (
-        <Rockets
-          key={item.id}
-          rocketImage={item.flickr_images[0]}
-          rocketName={item.rocket_name}
-          rocketDescription={item.description}
-        />
-      ))}
-    </section>
+    <>
+      <section className="Home">
+        <article className="intro">
+          <div className="introDet">
+            <p>So you want to Tarvel </p>
+            <h1>SPACE</h1>
+          </div>
+          <p>
+            Lets face it; if you want to go to space, you might as well genuinely
+            go to outer space and not hover kind of on the edge of it. Well sit
+            back, and relax because
+            we will give you a truly out of this world
+            experience!
+          </p>
+          <a href="#rockets" className="btn btn-special">Explore</a>
+        </article>
+
+      </section>
+      <section className="Rocket-List" id="rockets">
+        {rockets.map((item) => (
+          <Rockets
+            key={item.id}
+            rocketImage={item.flickr_images[0]}
+            rocketName={item.rocket_name}
+            rocketDescription={item.description}
+          />
+        ))}
+      </section>
+    </>
   );
 };
 export default RocketList;
