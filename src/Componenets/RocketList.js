2bf842f9a11cd24721e1d615d9c7f820932dcc19
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-09 16:04:31 +0500

    Style Homepage Details

--- src/Componenets/RocketList.js
+++ src/Componenets/RocketList.js
@@ -14,16 +14,16 @@ const RocketList = () => {
       <section className="Home">
         <article className="intro">
           <div className="introDet">
-            <p>So you want to Tarvel </p>
-            <h1>SPACE</h1>
+            <p className="det basicTXT">So you want to Tarvel </p>
+            <h1 className="heading">SPACE</h1>
+            <p className="Paragraph">
+              Lets face it; if you want to go to space, you might as well genuinely
+              go to outer space and not hover kind of on the edge of it. Well sit
+              back, and relax because
+              we will give you a truly out of this world
+              experience!
+            </p>
           </div>
-          <p>
-            Lets face it; if you want to go to space, you might as well genuinely
-            go to outer space and not hover kind of on the edge of it. Well sit
-            back, and relax because
-            we will give you a truly out of this world
-            experience!
-          </p>
           <a href="#rockets" className="btn btn-special">Explore</a>
         </article>
 
